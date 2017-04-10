7add60f3f5e31603ae0f0856bf242fb17ce2e499
Hikariii <sderooij.git@gmail.com>
2017-04-10 16:35:48 +0200

    variable to re-prefix resque processes

--- lib/Resque/Worker.php
+++ lib/Resque/Worker.php
@@ -326,7 +326,7 @@ class Resque_Worker
 	 */
 	private function updateProcLine($status)
 	{
-		$processTitle = 'resque-' . Resque::VERSION . ': ' . $status;
+		$processTitle = static::$processName . Resque::VERSION . ': ' . $status;
 		if(function_exists('cli_set_process_title')) {
 			cli_set_process_title($processTitle);
 		}
